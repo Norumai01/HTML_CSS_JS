3c96332d3df83c1596b5a5bd44794edbe11fc9ff
Norumai01 <johnnydinhnguyen2001@gmail.com>
2024-04-15 22:28:25 -0400

    Basic of Javascript.

---
+++ Foundational_Course/HTML_Tutorial/pages/jsBasic.js
@@ -0,0 +1,13 @@
+function HelloWorld() {
+    console.log("Hello World!");
+}
+
+function TextMessage() {
+    let name = "John Hoe", age = 25;
+    alert("My name is " + name + " and I am " + age + " years old.");
+}
+
+function MathOperation() {
+    let a = 8, b = 3;
+    console.log(a * b);
+}
